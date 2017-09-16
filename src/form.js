6bf44b60dedb8e237ded52ea3df51edfe1d91f34
Julian <julian@contingent.dk>
2017-09-16 22:26:14 +0200

    get picture from latest form

--- src/form.js
+++ src/form.js
@@ -7,8 +7,9 @@ angular.module('app').component('createForm', {
 
 <div layout="column" ng-cloak class="md-inline-form">
   <md-content layout-padding>
-    <div>
       <form name="userForm">
+        <h4>#1 Company owner</h4>
+          
         <div layout-gt-xs="row">
           <md-input-container class="md-block" flex-gt-xs>
             <label>Company CVR</label>
@@ -17,33 +18,86 @@ angular.module('app').component('createForm', {
         </div>
                 
         <div layout-gt-sm="row">
-            <div>
-                Owner(s) <span ng-bind="data.owners"></span>
+            <div ng-if="data.owners">
+                Virk.dk - owner(s) of CVR 28319436: <strong ng-bind="data.owners"></strong>
             </div>
         </div>                    
-
       </form>
-    </div>
   </md-content>
+  
+<md-content class="md-padding" layout-xs="column" layout="row">
+    <h4>#2 Picture of ID document</h4>
+</md-content>
+
+<md-content class="md-padding" layout-xs="column" layout="row">
+    <div flex-xs flex-gt-xs="50" layout="column">
+          <md-card>
+            <img ng-src="{{data.image}}" class="md-card-image" alt="image of id">
+            <!--<md-card-title>-->
+              <!--&lt;!&ndash;<md-card-title-text>&ndash;&gt;-->
+                <!--&lt;!&ndash;<span class="md-headline">Action buttons</span>&ndash;&gt;-->
+              <!--&lt;!&ndash;</md-card-title-text>&ndash;&gt;-->
+            <!--</md-card-title>-->
+                <md-card-content>
+                  <p>
+                      User-uploaded image.
+                  </p>
+            </md-card-content>       
+        </md-card>
+  </div>
+  
+</md-content>
+
+<md-content>
+  <p>
+      <md-button ng-click="$ctrl.print()" class="md-raised">Print as PDF</md-button>
+  </p>
+</md-content>
 
 </div>       
 `,
     controller: function ($scope) {
         const data = this.data = $scope.data = {
             id: 28319436,
+            cvr: 28319436,
             date: new Date().toDateString(),
+            // image: `//api.typeform.com/v1/form/DZ1iMw?key=${sessionStorage.key}&completed=true`,
+            image: ``,
         }
 
-        this.lookupCvr = (newVal) => {
-            fetch(`https://cvrapi.dk/api?search=${newVal}&country=dk`)
-                .then(res => {
-                    return res.json()
-                },
-                reason => {
-                    console.warn(reason)
+        this.getLatestResponse = () => {
+            const url = `https://api.typeform.com/v1/form/DZ1iMw?key=${sessionStorage.apikey}&completed=true`
+            fetch(url)
+                .then(res => res.json(),
+                    reason => console.warn(reason)
+                )
+                .then(json => {
+                    $scope.$apply(() => {
+                        console.debug(json.responses)
+                        if (json.responses instanceof Array && json.responses.length > 0) {
+                            const latest = json.responses[json.responses.length-1]
+
+                            console.debug(latest)
+                            data.image = latest.answers.fileupload_61070177
+                            data.cvr = parseInt(latest.answers.textfield_61074806, 10)
+                            this.lookupCvr(data.cvr)
+                        }
+                    })
                 })
-            .then(json => { processCvrData(json) })
-            .catch(error => console.warn(error))
+                .catch(error => console.warn(error))
+        }
+
+        setTimeout(()=>{        this.getLatestResponse()
+        },1)
+
+        this.lookupCvr = (newVal) => {
+            console.debug(`lookup cvr...`)
+            fetch(`https://cvrapi.dk/api?search=${newVal}&country=dk`)
+                .then(res => res.json(),
+                      reason => console.warn(reason)
+                )
+                .then(json => { processCvrData(json) })
+                .catch(error => console.warn(error))
         }
 
         function processCvrData (apiData) {
@@ -55,5 +109,7 @@ angular.module('app').component('createForm', {
                 }
             })
         }
+
+        this.print = () => {window.print()}
     },
 })
